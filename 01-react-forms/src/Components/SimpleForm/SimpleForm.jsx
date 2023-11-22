e7e1ee6246e4fa7c1d3119ac9f5ec319c798fe7f
jesualujan <lujan.oficial@gmail.com>
2023-11-21 21:52:55 -0600

    ✨ creación del componente y del estado de email & password

--- 01-react-forms/src/Components/SimpleForm/SimpleForm.jsx
+++ 01-react-forms/src/Components/SimpleForm/SimpleForm.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import logo from '../assets/react.svg'
+import { useState } from 'react'
 
 const SimpleForm = () => {
+ //* TODA NUESTRA LÓGICA VA AQUÍ PARA QUE FUNCIONE NUESTRO FORM 
+ //? PASO #2: CREAR LOS ESTADOS DONDE GUARDARÉ LA INFORMACIÓN TECLEADA     
+   const [email, setEmail] = useState ('')
+   const [password, setPassword] = useState('')
+
+   //? PASO #4: Crear la función que se ejecutara al hacer click en el botón de enviar del formulario
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log('Email: ' ,email ,'Password: ' , password)
+    }
+
   return (
-    <div>SimpleForm</div>
+    //? PASO #1: CREAR EL FORMULARIO BASE JSX
+    <div className='login'>
+    <div className='login-container'>
+      <img src={logo} alt='logo' />
+      
+      <form onSubmit={handleSubmit}>
+        {/* PASO #3: GUARDAR LA INFORMACION EN EL ESTADO DE  */}
+        <label htmlFor='email'>Email</label>
+        <input
+          type='text'
+          name='email'
+          placeholder='correo@mail.com'
+          id='simple-email'
+          onChange={(event) => setEmail(event.target.value) }
+          value={email}
+        />
+
+        <label htmlFor='password'>Password</label>
+        <input
+          type='password'
+          name='password'
+          id='simple-password'
+          onChange={(event) => setPassword(event.target.value) }
+          value={password}
+        />
+        
+        <button type='submit'>
+          Iniciar Sesion
+        </button>
+        
+      </form>
+    </div>
+  </div>
   )
 }
 
