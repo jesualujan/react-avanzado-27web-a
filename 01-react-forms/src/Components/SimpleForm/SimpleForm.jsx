103cae6fcaea180801d06c69a67a31511ff38ff8
jesualujan <lujan.oficial@gmail.com>
2023-11-23 22:38:55 -0600

    🐛 corregi el error de logo

--- 01-react-forms/src/Components/SimpleForm/SimpleForm.jsx
+++ 01-react-forms/src/Components/SimpleForm/SimpleForm.jsx
@@ -1,4 +1,4 @@
-
+import logo from '../../assets/react.svg'
 import { useState } from 'react'
 
 const SimpleForm = () => {
@@ -17,8 +17,8 @@ const SimpleForm = () => {
     //? PASO #1: CREAR EL FORMULARIO BASE JSX
     <div className='login'>
     <div className='login-container'>
-      
-      
+   <br />
+    <img src={logo} alt='logo' />  
       <form onSubmit={handleSubmit}>
         {/* PASO #3: GUARDAR LA INFORMACION EN EL ESTADO DE  */}
         <label htmlFor='email'>Email</label>
