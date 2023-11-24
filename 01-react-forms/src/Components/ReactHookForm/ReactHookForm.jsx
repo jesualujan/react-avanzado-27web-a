17545bc88792e899b9c5976c107c172d2afde6fb
jesualujan <lujan.oficial@gmail.com>
2023-11-23 19:18:03 -0600

    ✨ crear mis reglas de validación del form con yup

--- 01-react-forms/src/Components/ReactHookForm/ReactHookForm.jsx
+++ 01-react-forms/src/Components/ReactHookForm/ReactHookForm.jsx
@@ -1,4 +1,4 @@
-import logo from '../assets/react.svg'
+
 import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
@@ -9,27 +9,36 @@ const ReactHookForm = () => {
     //* NUESTRA LÓGICA | CREACION DEL ESQUEMA DEL FORMULARIO DE USUARIO UTILANZO YUP, PARA VALIDAR NUESTROS CAMPOS 
 
      const userFormSchema = yup.object({
-        firstName: yup.string().required('Escribe tu nombre'),
-        lastName: yup.string().required('Escribe tu apellido') ,
-        age: yup.number().positive('La edad debe ser un número entero positivo').integer('Ingresa tu edad'),
-        password: yup.string().required('No ingresaste una contraseña')
-        .min(5, 'La contraseña es muy corta, debe tener al menos 5 caracteres')
-        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%.^&*])/, 
-      'La contraseña debe tener al menos 5 caracteres, un número, una letra mayúscula, una letra minúscula y un caracter especial'),
-        gender: yup 
-        .mixed()
-        .oneOf(['M', 'F', 'O'], 'Selecciona tu género')
-        .defined(),
-     }).required()
+      firstName: yup.string().required('Escribe tu nombre'),
+      lastName: yup.string().required('Escribe tu apellido') ,
+      age: yup.number().positive('La edad de ser un número positivo').integer('Ingresa tu edad'),
+      password: yup.string().required('No ingresaste tu contraseña')
+      .min(5, 'La contraseña es muy corta, debe tener al menos 5 caracteres')
+      .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%.^&*])/, 
+      'La contraseña debe tener al menos 5 caracteres, un número, una letra mayúscula, una letra minúscula y un caracter especial')
+      ,
+      gender: yup
+      .mixed()
+      .oneOf(['M', 'F', 'O'], 'Selecciona un género: Hombre, Mujer u Otro')   
+      .defined()   
+      ,
+  }).required()
+  
+     const { register, handleSubmit, formState:{ errors } } = useForm({
+      //RESOLVER SIRVE PARA ESTABLECER EL ESQUEMA DE VALIDACIÓN,
+      // PARA ELLO USAMOS LA FUNCIÓN YUPRESOLVER Y LE PASAMOS COMO ARGUMENTO NUESTRO SCHEMA
+      resolver: yupResolver(userFormSchema)
+    });
+    const onSubmit = data => console.log(data);
 
   return (
     //* VA TODO NUESTRO FORMULARIO DE REGISTRO EN BASE JSX
     //? Paso #1: Crear el formulario base en JSX
     <div className='login'>
       <div className='login-container'>
-        <img src={logo} alt='logo' />
+       
         <form
-          onSubmit={() => { }/* HANDLE SUBMIT */}
+          onSubmit={handleSubmit(onSubmit)}
           style={{ display: 'flex', flexDirection: 'column' }}
         >
         
@@ -39,7 +48,9 @@ const ReactHookForm = () => {
             name='firstName'
             placeholder='Tu Nombre'
             id='firstName'
+            {...register('firstName', {required: true , maxLength: 20})}
           />
+          <p>{errors.firstName?.message}</p>
 
           <label htmlFor='lastName'>Apellido</label>
           <input
@@ -47,7 +58,9 @@ const ReactHookForm = () => {
             name='lastName'
             placeholder='Tu Apellido'
             id='lastName'
+            {...register('lastName', {pattern: /^[A-Za-z]+$/i})}
           />
+          <p>{errors.lastName?.message}</p>
 
           <label htmlFor='age'>Edad</label>
           <input
@@ -55,7 +68,9 @@ const ReactHookForm = () => {
             name='age'
             placeholder='Tu Edad'
             id='age'
+            {...register('age')}
           />
+          <p>{errors.age?.message}</p>
 
           <label htmlFor='gender'>Genero</label>
           <select name='gender' id='gender'>
@@ -64,6 +79,7 @@ const ReactHookForm = () => {
             <option value='F'>Femenino</option>
             <option value='O'>Otro</option>
           </select>
+          <p>{errors.gender?.message}</p>
 
           <label htmlFor='email'>Email</label>
           <input
@@ -71,14 +87,18 @@ const ReactHookForm = () => {
             name='email'
             placeholder='correo@mail.com'
             id='email'
+            {...register('email')}
           />
+          <p>{errors.email?.message}</p>
 
           <label htmlFor='password'>Password</label>
           <input
             type='password'
             name='password'
             id='password'
+            {...register('password')}
           />
+           <p>{errors.email?.message}</p>
 
           <button type='submit'>
             Registrar
