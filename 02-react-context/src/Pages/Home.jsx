dd3c07bb262acb37a20bfc82df87b55e1b766f79
jesualujan <lujan.oficial@gmail.com>
2023-11-30 14:14:47 -0600

    ✨ Importamos a Header y lo agregamos al Provider

--- 02-react-context/src/Pages/Home.jsx
+++ 02-react-context/src/Pages/Home.jsx
@@ -2,17 +2,19 @@ import SongList from '@/Components/SongList/SongList'
 import SongDetail from '@/Components/SongDetail/SongDetail'
 import './home.css'
 import { SongProvider } from '@/Context/SongContext'
+import Header from '@/Components/Header'
 
 const Home = () => {
   return (
     <SongProvider>
+      <Header />
       <div className='home-container'>
         <div className='left'>
-          <p>lado Izquierdo</p>
+          <p>lado Izquierdo </p>
           <SongList />
         </div>
         <div className='right'>
-          <p>lado Derecho</p>
+          <p>lado Derecho </p>
           <SongDetail />
         </div>
       </div>
