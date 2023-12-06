db249644fb11b247a68efef164e569e16636196d
jesualujan <lujan.oficial@gmail.com>
2023-12-05 21:52:32 -0600

    ✨ utilizamos react hook form y dejamos listo el form

--- 03-react-auth/src/Pages/Login.jsx
+++ 03-react-auth/src/Pages/Login.jsx
@@ -1,28 +1,52 @@
 import logo from '@/assets/react.svg'
+import { useForm } from 'react-hook-form'
 import '@/styles/form.css'
+// import { loginUserService } from '@/Service/userServices'
 
 const Login = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm()
+  const onSubmit = data => console.log(data)
+
   return (
     <main className='form-signin w-100 m-auto'>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <img
           className='mb-4'
-          src={logo}
-          alt='Logo React'
-          width='72'
-          height='57'
+          src={logo} alt='React'
+          width={72}
+          height={57}
         />
-        <h1 className='h3 mb-3 fw-normal'>Please sign in</h1>
+        <h1 className='h3 mb-3 fw-normal'>Please Sign in</h1>
+
         <div className='form-floating'>
-          <input type='email' className='form-control' id='floatingInput' placeholder='name@example.com' />
+          <input
+            type='email'
+            className='form-control'
+            name='email'
+            id='floatingInput'
+            placeholder='name@example.com'
+            {...register('email')}
+          />
+          <p>{errors.email?.message}</p>
           <label htmlFor='floatingInput'>Email address</label>
         </div>
+
         <div className='form-floating'>
-          <input type='password' className='form-control' id='floatingPassword' placeholder='Password' />
+          <input
+            type='password'
+            className='form-control'
+            name='password'
+            id='floatingPassword'
+            placeholder='Password'
+            {...register('password')}
+          />
+          <p>{errors.password?.message}</p>
           <label htmlFor='floatingPassword'>Password</label>
         </div>
 
-        <button className='btn btn-primary w-100 py-2' type='submit'>Sign in</button>
+        <button className='btn btn-primary w-100 py-2' type='submit'>
+          Sign in
+        </button>
         <p className='mt-5 mb-3 text-body-secondary'>© 2017–2023</p>
       </form>
     </main>
