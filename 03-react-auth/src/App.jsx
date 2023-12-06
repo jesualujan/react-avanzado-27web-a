c5d58ab90d8f5d909d282d9abfc339dfcdc26063
jesualujan <lujan.oficial@gmail.com>
2023-12-05 19:50:19 -0600

    ✨ llamar a los componentes de la carpeta Pages

--- 03-react-auth/src/App.jsx
+++ 03-react-auth/src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import { Home, Dashboard, Login, Secret, Signup } from '@/Pages'
 import Header from '@/Components/Header'
 
 function App () {
   return (
     <>
       <Header />
+      <p> React Auth</p>
+      <Home />
+      <Dashboard />
+      <Login />
+      <Secret />
+      <Signup />
     </>
   )
 }
