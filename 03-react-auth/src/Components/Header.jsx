19a0e2e3b3927ad12fba6f697d9450a1e48de5bb
jesualujan <lujan.oficial@gmail.com>
2023-12-05 21:55:30 -0600

    🐛 arregle un pequeño bug en los estilos de header

--- 03-react-auth/src/Components/Header.jsx
+++ 03-react-auth/src/Components/Header.jsx
@@ -1,17 +1,17 @@
 import './header.scss'
 import { NavLink } from 'react-router-dom'
 // Documentación de NavLink: https://reactrouter.com/en/main/components/nav-link
-// NavLink es un tipo especial de link, que me permite gestionar estilos en función de la ruta activa (isActive)
+// NavLink es un tipo especial de Link, que me permite gestionar estilos en función
+// de la ruta activa (isActive)
 
 const Header = () => {
   const linkIsActive = (isActive) => {
     return isActive ? 'header__item-link header__item-link--is-active' : 'header__item-link'
   }
-
   return (
-    // BEM PARA NOMBRAR O ASIGNAR UN NOMBRE A NUESTRA className
-    <nav className='header'> {/* Bloque */}
-      <NavLink className='header__logo' to='/'>Logo</NavLink>
+  // bloques para BEM, COMO LLAMAR NUESTRAS CLASES
+    <nav className='header'>
+      <NavLink className='header__logo' to='/'>LOGO</NavLink>
       <ul className='header__nav-list'>
         <li className='header__list-item'>
           <NavLink className={({ isActive }) => linkIsActive(isActive)} to='/'>Home</NavLink>
@@ -23,7 +23,7 @@ const Header = () => {
           <NavLink className={({ isActive }) => linkIsActive(isActive)} to='/secret'>Secret</NavLink>
         </li>
         <li className='header__list-item'>
-          <NavLink className={({ isActive }) => linkIsActive(isActive)} to='/login'>Loging</NavLink>
+          <NavLink className={({ isActive }) => linkIsActive(isActive)} to='/login'>Login</NavLink>
         </li>
         <li className='header__list-item'>
           <NavLink className={({ isActive }) => linkIsActive(isActive)} to='/signup'>Signup</NavLink>
