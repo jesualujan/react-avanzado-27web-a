0887d124388c9d8ff9a72571c848dbc7c1aa1ea5
jesualujan <lujan.oficial@gmail.com>
2023-12-05 20:12:26 -0600

    ✨ creamos nuestras rutas de nuestro sitio web

---
+++ 03-react-auth/src/Routes/Routes.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+import { Home, Dashboard, Login, Secret, Signup } from '@/Pages'
+
+const RoutesIndex = () => {
+  return (
+    // RENDERIZAMOS LOS COMPONENTES
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/secret' element={<Secret />} />
+      <Route path='/signup' element={<Signup />} />
+    </Routes>
+  )
+}
+
+export default RoutesIndex
