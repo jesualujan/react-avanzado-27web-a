516544e64ef35810fffbbc10502c9f2cf0ae6175
jesualujan <lujan.oficial@gmail.com>
2023-12-05 21:50:24 -0600

    ✨ usamos NavLink en vez de <a>

--- 03-react-auth/src/Components/Header.jsx
+++ 03-react-auth/src/Components/Header.jsx
@@ -1,25 +1,32 @@
 import './header.scss'
+import { NavLink } from 'react-router-dom'
+// Documentación de NavLink: https://reactrouter.com/en/main/components/nav-link
+// NavLink es un tipo especial de link, que me permite gestionar estilos en función de la ruta activa (isActive)
 
 const Header = () => {
+  const linkIsActive = (isActive) => {
+    return isActive ? 'header__item-link header__item-link--is-active' : 'header__item-link'
+  }
+
   return (
     // BEM PARA NOMBRAR O ASIGNAR UN NOMBRE A NUESTRA className
     <nav className='header'> {/* Bloque */}
-      <a className='header__logo' href='/'>Logo</a>
+      <NavLink className='header__logo' to='/'>Logo</NavLink>
       <ul className='header__nav-list'>
         <li className='header__list-item'>
-          <a className='header__item-link--is-active' href='/home'>Home</a>
+          <NavLink className={({ isActive }) => linkIsActive(isActive)} to='/'>Home</NavLink>
         </li>
         <li className='header__list-item'>
-          <a className='header__item-link' href='/dashboard'>Dashboard</a>
+          <NavLink className={({ isActive }) => linkIsActive(isActive)} to='/dashboard'>Dashboard</NavLink>
         </li>
         <li className='header__list-item'>
-          <a className='header__item-link' href='/secret'>Secret</a>
+          <NavLink className={({ isActive }) => linkIsActive(isActive)} to='/secret'>Secret</NavLink>
         </li>
         <li className='header__list-item'>
-          <a className='header__item-link' href='/login'>Loging</a>
+          <NavLink className={({ isActive }) => linkIsActive(isActive)} to='/login'>Loging</NavLink>
         </li>
         <li className='header__list-item'>
-          <a className='header__item-link' href='/signup'>Signup</a>
+          <NavLink className={({ isActive }) => linkIsActive(isActive)} to='/signup'>Signup</NavLink>
         </li>
       </ul>
     </nav>
