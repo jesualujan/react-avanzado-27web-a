9bdb1992d2e359deabc5a0ac2956a5799dd2c865
jesualujan <lujan.oficial@gmail.com>
2023-12-07 21:01:05 -0600

    ✨ renderizado condicional especializado

--- 03-react-auth/src/Pages/Secret.jsx
+++ 03-react-auth/src/Pages/Secret.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { useAuthContext } from '@/Hook/useAuthContext'
 
 const Secret = () => {
+// lo ocupamos puesto que tiene el token decodificado
+  const { userPayload } = useAuthContext()
   return (
-    <h3>Secret</h3>
+    <>
+      <h1>Mensaje de Bienvenida</h1>
+      {
+      userPayload?.role === 'ADMIN'
+        ? <h2>Hola Admin</h2>
+        : <h2>Hola</h2>
+    }
+      {userPayload?.role === 'ADMIN' && <h2> Saludos Admin</h2>}
+      {userPayload?.role === 'CUSTOMER' && <h2> Saludos Customer</h2>}
+    </>
   )
 }
 
