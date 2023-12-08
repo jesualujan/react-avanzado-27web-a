949cc82fc14633ec4181385fbac0d0aaa95141ed
jesualujan <lujan.oficial@gmail.com>
2023-12-07 21:19:16 -0600

    ✨ mostrar los datos del usuario

--- 03-react-auth/src/Pages/Dashboard.jsx
+++ 03-react-auth/src/Pages/Dashboard.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { getMeUserServices } from '@/Service/userServices'
 
 const Dashboard = () => {
+  // en el estado
+  const [userData, setUserData] = useState({}) // vacío
+  const token = localStorage.getItem('token')
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await getMeUserServices(token)
+        if (response.status === 200) {
+          setUserData(response.data)
+        }
+      } catch (error) {
+        console.log('Ocurrio un error en Dashboard', error)
+      }
+    }
+    fetchUserData()
+  }, [token])
+
   return (
-    <h3>Dashboard</h3>
+    <>
+      <h3>Bienvido a tu perfil</h3>
+      <p> Aca ustedes pueden agregar estilos bootstrap, scss para crearle un card al usuario </p>
+      {
+        userData?.first_name && <p>{userData.first_name}</p>
+      }
+      {
+        userData?.last_name && <p>{userData.last_name}</p>
+      }
+      {
+        userData?.gender && <p>{userData.gender}</p>
+      }
+      {
+        userData?.email && <p>{userData.email}</p>
+      }
+      {
+        userData?.role && <p>{userData.role}</p>
+      }
+    </>
   )
 }
 
